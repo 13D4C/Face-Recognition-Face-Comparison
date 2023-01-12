a4fb65dfe55a51f434bae4913b35a52ba88609aa
D4C <115446911+13D4C@users.noreply.github.com>
2023-01-12 19:59:46 +0700

    Update Face Recognition Face Comparison.py

--- Face Recognition Face Comparison.py
+++ Face Recognition Face Comparison.py
@@ -1,18 +1,18 @@
-//เราจะใช้ 1 Library
-นั้นก็คือ face_recognition และโหลดโมเดลมาใช้//
+#เราจะใช้ 1 Library
+#นั้นก็คือ face_recognition และโหลดโมเดลมาใช้
 
 import face_recognition as face
 from face_recognition.api import face_encoding
 
-//สำหรับนำรูปเข้าData base ของโมเดล//
+#สำหรับนำรูปเข้าData base ของโมเดล
 database_image = face_recognition.load_image_flie('รูปของคุณ')
 database_encoding = face_recognition.face_encordings(database_image)[0]
 
-//นำรูปมาเปรียบเทียบ//
+#นำรูปมาเปรียบเทียบ
 sample_image = face_recognition.load_image_flie('รูปที่เราจะเอามาเปรียบเทียบ')
 sample_encoding = face_recognition.face_encording(sample_image)[0]
 
-//ตัวแสดงผลลัพธ์//
+#ตัวแสดงผลลัพธ์
 result = face_recognition.compare_faces([database_encoding],sample_encoding)
 if result[0] == True:
     print("ใช่! รูปนี้คือคนเดียวกัน")
